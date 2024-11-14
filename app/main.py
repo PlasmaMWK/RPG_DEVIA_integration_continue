e7e6b92ed08823cef5e128a2769bffadf9e7c7ac
PlasmaMWK <72070358+PlasmaMWK@users.noreply.github.com>
2024-11-14 12:00:04 +0100

    OK

--- app/main.py
+++ app/main.py
@@ -18,16 +18,7 @@ class Personnage:
     def est_blesse(self):
         if self.hp < 10 and self.hp > 0:
             return True
-<<<<<<< HEAD
-<<<<<<< HEAD
-        return False
-    
-    #Coucou c'est un test 4
-=======
-        return False
->>>>>>> 3e5da20 ([GREEN] test est blesse OK)
-=======
         return False
     
     #Coucou c'est un test 5
->>>>>>> 5fd32247fce327e9517a3bbbe11550b722ca0da6
+
